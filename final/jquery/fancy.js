03a879ceefc2cd3fb4c7b23865ec9da7c4d295d9
danaeinsidler <Dana.Einsidler@massart.edu>
2014-11-17 13:13:43 -0500

    jquery addition and logo fix

---
+++ final/jquery/fancy.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+	$(".fancybox-button").fancybox({
+		prevEffect		: 'none',
+		nextEffect		: 'none',
+		closeBtn		: false,
+		helpers		: {
+			title	: { type : 'inside' },
+			buttons	: {}
+		}
+	});
+});
+
+
+
+
+
